852abe8256e33e5b270cdb467484d94df10a240d
Emil Ohlsson <emil@kottland.net>
2018-02-26 21:45:14 +0100

    Outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,9 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
-
-    ### your code goes here
-
+    l = len(predictions)
+    errs = [(a[0], nw[0], abs(nw[0] - p[0])) for a, nw, p in zip(ages, net_worths, predictions)]
+    cleaned_data = sorted(errs, key=lambda v: v[2])[0:l - l/10]
     
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,14 +26,13 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn.linear_model import LinearRegression as LR
 
+reg = LR()
 
+reg.fit(ages_train, net_worths_train)
 
-
-
-
-
-
+print "Uncleansed: ", reg.coef_, reg.score(ages_test, net_worths_test)
 
 
 try:
@@ -54,11 +53,6 @@ except NameError:
     print "can't make predictions to use in identifying outliers"
 
 
-
-
-
-
-
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
@@ -68,6 +62,7 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print "Cleansed: ", reg.coef_, reg.score(ages_test, net_worths_test)
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
