6be91f4952d6d4887828c5f8f241cbf4130ade8b
Emil Ohlsson <emil@kottland.net>
2018-03-05 21:08:33 +0100

    Evaluation metrices

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -16,6 +16,10 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
+from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
+
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
 ### add more features to features_list!
@@ -27,5 +31,24 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+### it's all yours from here forward!  
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features,labels, random_state=42, test_size=.3)
+clf = DTC()
+clf.fit(features_train, labels_train)
 
+predictions = clf.predict(features_test)
+score = clf.score(features_test, labels_test)
+print "Accuracy:", score
+print sum(predictions), "predicted in the test set"
 
+print confusion_matrix(labels_test, predictions)
+print "precision", precision_score(labels_test, predictions)
+print "recall", recall_score(labels_test, predictions)
+
+preds = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+actuals = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+print confusion_matrix(actuals, preds)
+print "precision:", precision_score(actuals, preds)
+print "recall:", recall_score(actuals, preds)
--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -66,7 +66,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 250
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
@@ -80,6 +80,9 @@ t0 = time()
 X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 print "done in %0.3fs" % (time() - t0)
+
+print "First:", pca.explained_variance_ratio_[0]
+print "First:", pca.explained_variance_ratio_[1]
 
 
 ###############################################################################
@@ -136,7 +139,7 @@ def title(y_pred, y_test, target_names, i):
 prediction_titles = [title(y_pred, y_test, target_names, i)
                          for i in range(y_pred.shape[0])]
 
-plot_gallery(X_test, prediction_titles, h, w)
+#plot_gallery(X_test, prediction_titles, h, w)
 
 # plot the gallery of the most significative eigenfaces
 
